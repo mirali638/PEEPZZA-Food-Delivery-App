8c96d0c118455dc8d2c8c3bbbb3f4a90b1d9a3b8
Mirali Jivani <miralijivani632@gmail.com>
2025-10-05 11:02:39 +0530

    create home page

---
+++ frontend/src/components/UserDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function userDashboard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default userDashboard
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import UserDashboard from '../components/UserDashboard.jsx'
+import OwnerDashboard from '../components/OwnerDashboard.jsx'
+import DeliveryBoy from '../components/DeliveryBoy.jsx'
 
 function Home() {
+    const {userData}=useSelector(state=>state.user)
   return (
-    <div>
-      
+    <div className='w-[100vw] min-h-[100vh] pt-[100px] flex flex-col items-center bg-[#fff9f6]'>
+      {userData.role=="user" && <UserDashboard/>}
+      {userData.role=="owner" && <OwnerDashboard/>}
+      {userData.role=="deliveryBoy" && <DeliveryBoy/>}
     </div>
   )
 }
